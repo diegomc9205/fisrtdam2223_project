3d56fc2648819261ef2aa1feaa80cc80e2c3a53e
diegomc9205 <114580774+diegomc9205@users.noreply.github.com>
2022-11-09 14:35:07 +0100

    9.11

--- Assets/scripts/ej4.cs
+++ Assets/scripts/ej4.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class reboteuno : MonoBehaviour
+public class ej4 : MonoBehaviour
 {
     private float speed=5;
-    private float toplimit = 10;
-    private float minlimit = -10;
+    private float toplimit = 5;
+    private float minlimit = -5;
 
     private Vector3 direction = Vector3.up;
+    private Vector3 directionHorizontal = Vector3.right;
+ 
     void Update()
     {
 
         Vector3 posicionactual = transform.position;
         if (posicionactual.y > toplimit || posicionactual.y < minlimit)
             direction.y *= -1;
+            directionHorizontal.x *= 1;
+   
+
+
         Vector3 velocity = direction * speed;//pienso en terminos de velocidad por segundo
+       
         Vector3 displacement = velocity * Time.deltaTime;//pienso en velocidad instantanea entre dos espacios de tiempo muy pequeños
         transform.position +=  displacement;//actualizo la posicion( translación)
 
--- Assets/scripts/rebotedos.cs
+++ Assets/scripts/rebotedos.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class reboteuno : MonoBehaviour
+public class rebotedos : MonoBehaviour
 {
-    private float speed=5;
-    private float toplimit = 10;
-    private float minlimit = -10;
+    private float speed=2;
+    private float toplimit = 5;
+    private float minlimit = -5;
 
     private Vector3 direction = Vector3.up;
+ 
     void Update()
     {
 
         Vector3 posicionactual = transform.position;
         if (posicionactual.y > toplimit || posicionactual.y < minlimit)
             direction.y *= -1;
+
+
         Vector3 velocity = direction * speed;//pienso en terminos de velocidad por segundo
+        if (direction.y > 0)
+            velocity = velocity * 2;
         Vector3 displacement = velocity * Time.deltaTime;//pienso en velocidad instantanea entre dos espacios de tiempo muy pequeños
         transform.position +=  displacement;//actualizo la posicion( translación)
 
